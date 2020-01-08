9834ca25aff6758f11bf5098db4ae725f2aa04b7
Chris Johnson <chrisjohnson0@gmail.com>
2020-01-08 10:50:09 -0500

    Rename worker->sinkworker

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/chrisjohnson/azure-key-vault-agent/config"
 	"github.com/chrisjohnson/azure-key-vault-agent/sink"
-	"github.com/chrisjohnson/azure-key-vault-agent/worker"
+	"github.com/chrisjohnson/azure-key-vault-agent/sinkworker"
 )
 
 func init() {
@@ -31,13 +31,13 @@ func main() {
 	// TODO: Parse config once to determine these
 	// TODO: Set up a file watch on the config that parses, cancels the existing workers, and starts a new set
 
-	cfg1 := sink.SinkConfig{Name: "password", Frequency: 1, Type: sink.SecretType, VaultBaseURL: "https://cjohnson-kv.vault.azure.net/"}
-	cfg2 := sink.SinkConfig{Name: "cjohnson-test", Frequency: 1, Type: sink.CertType, VaultBaseURL: "https://cjohnson-kv.vault.azure.net/"}
-	//cfg3 := sink.SinkConfig{Name: "C", Frequency: 1, Type: worker.CertType, VaultBaseURL: "https://cjohnson-kv.vault.azure.net/"}
+	cfg1 := sink.SinkConfig{Name: "username", Frequency: 1, Type: sink.SecretType, VaultBaseURL: "https://cjohnson-kv.vault.azure.net/"}
+	cfg2 := sink.SinkConfig{Name: "password", Frequency: 1, Type: sink.SecretType, VaultBaseURL: "https://cjohnson-kv.vault.azure.net/"}
+	cfg3 := sink.SinkConfig{Name: "cjohnson-test", Frequency: 1, Type: sink.CertType, VaultBaseURL: "https://cjohnson-kv.vault.azure.net/"}
 
-	go worker.Worker(ctx, cfg1)
-	go worker.Worker(ctx, cfg2)
-	//go worker.Worker(ctx, cfg3)
+	go sinkworker.Worker(ctx, cfg1)
+	go sinkworker.Worker(ctx, cfg2)
+	go sinkworker.Worker(ctx, cfg3)
 
 	// Run for 15 seconds, then stop
 	time.Sleep(15 * time.Second)
--- sinkworker/sinkworker.go
+++ sinkworker/sinkworker.go
@@ -1,4 +1,4 @@
-package worker
+package sinkworker
 
 import (
 	"context"
