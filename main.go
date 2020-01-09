c19a9894196506d11f6f046ffc736833ab2b523e
grep-ir <valentine.181@gmail.com>
2020-01-09 13:12:36 -0500

    cleanup

--- main.go
+++ main.go
@@ -23,20 +23,4 @@ func init() {
 
 func main() {
 	configwatcher.ConfigWatcher("akva.yaml")
-
-	/*
-	cfg1 := sink.SinkConfig{Name: "username", Frequency: 1, Kind: sink.SecretKind, VaultBaseURL: "https://cjohnson-kv.vault.azure.net/"}
-	cfg2 := sink.SinkConfig{Name: "password", Frequency: 1, Kind: sink.SecretKind, VaultBaseURL: "https://cjohnson-kv.vault.azure.net/"}
-	cfg3 := sink.SinkConfig{Name: "cjohnson-test", Frequency: 1, Kind: sink.CertKind, VaultBaseURL: "https://cjohnson-kv.vault.azure.net/"}
-
-	go sinkworker.Worker(ctx, cfg1)
-	go sinkworker.Worker(ctx, cfg2)
-	go sinkworker.Worker(ctx, cfg3)
-
-	// Run for 15 seconds, then stop
-	time.Sleep(15 * time.Second)
-	log.Println("Shutting down")
-	cancel()
-	8
-	 */
 }
