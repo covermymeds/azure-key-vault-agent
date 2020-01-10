2b34d9ef1fcc025647e5cf72700e1511985845d2
grep-ir <valentine.181@gmail.com>
2020-01-10 09:29:42 -0500

    implementing very simple required field validation

--- sink/sink.go
+++ sink/sink.go
@@ -13,11 +13,11 @@ const (
 )
 
 type SinkConfig struct {
-	Kind         SinkKind      `yaml:"kind,omitempty"`
-	VaultBaseURL string        `yaml:"vaultBaseURL,omitempty"`
-	Name         string        `yaml:"name,omitempty"`
+	Kind         SinkKind      `yaml:"kind,omitempty" validate:"required"`
+	VaultBaseURL string        `yaml:"vaultBaseURL,omitempty" validate:"required"`
+	Name         string        `yaml:"name,omitempty" validate:"required"`
 	Version      string        `yaml:"version,omitempty"`
-	Path         string        `yaml:"path,omitempty"`
+	Path         string        `yaml:"path,omitempty" validate:"required"`
 	Frequency    time.Duration `yaml:"frequency,omitempty"`
 	Template     string        `yaml:"template,omitempty"`
 	TemplatePath string        `yaml:"templatePath,omitempty"`
