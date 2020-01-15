f3f05553ed34a6f96f98690ddf05aa0d8ac90b3b
grep-ir <valentine.181@gmail.com>
2020-01-15 16:02:18 -0500

    first stab at templating

---
+++ templateparser/templateparser.go
@@ -0,0 +1,46 @@
+package templateparser
+
+import (
+	"github.com/Masterminds/sprig"
+	"github.com/chrisjohnson/azure-key-vault-agent/resource"
+	"log"
+	"os"
+	"text/template"
+)
+
+func InlineTemplate(inline string, path string, resource resource.Resource) {
+	// read in the template file
+	t := template.Must(template.New("inline").Funcs(sprig.TxtFuncMap()).Parse(inline))
+
+	// create the destination file
+	f, err := os.Create(path)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer f.Close()
+
+	// Execute the template
+	err = t.Execute(f, resource.Map())
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func TemplateFile(tpath string, path string, resource resource.Resource) {
+	// read in the template file
+	t := template.Must(template.New(tpath).Funcs(sprig.TxtFuncMap()).ParseFiles(tpath))
+
+	// create the destination file
+	f, err := os.Create(path)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer f.Close()
+
+	// Execute the template
+	err = t.Execute(f, resource.Map())
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
