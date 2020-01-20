28011677549ead6ff52b8aca49fd42d7d2172d57
Chris Johnson <chrisjohnson0@gmail.com>
2020-01-20 13:22:44 -0500

    Start working on changing the config around to support our batch workers refactor

--- sink/sink.go
+++ sink/sink.go
@@ -10,16 +10,28 @@ const (
 	SecretKind SinkKind = "secret"
 )
 
+type ConfigFile struct {
+	Workers []WorkerConfig `yaml:"workers,omitempty" validate:"required"`
+}
+
+type WorkerConfig struct {
+	Resources     []ResourceConfig `yaml:"resources,omitempty" validate:"required"`
+	Frequency     string           `yaml:"frequency,omitempty" validate:"required"`
+	TimeFrequency time.Duration    `yaml:"timefrequency" validate:"-"`
+	PreChange     string           `yaml:"preChange,omitempty"`
+	PostChange    string           `yaml:"postChange,omitempty"`
+	Sinks         []SinkConfig     `yaml:"sinks,omitempty" validate:"required"`
+}
+
+type ResourceConfig struct {
+	Kind         SinkKind `yaml:"kind,omitempty" validate:"required,oneof=cert key secret"`
+	VaultBaseURL string   `yaml:"vaultBaseURL,omitempty" validate:"required,url"`
+	Name         string   `yaml:"name,omitempty" validate:"required"`
+	Version      string   `yaml:"version,omitempty"`
+}
+
 type SinkConfig struct {
-	Kind          SinkKind      `yaml:"kind,omitempty" validate:"required,oneof=cert key secret"`
-	VaultBaseURL  string        `yaml:"vaultBaseURL,omitempty" validate:"required,url"`
-	Name          string        `yaml:"name,omitempty" validate:"required"`
-	Version       string        `yaml:"version,omitempty"`
-	Path          string        `yaml:"path,omitempty" validate:"required"`
-	Frequency     string        `yaml:"frequency,omitempty" validate:"required"`
-	Template      string        `yaml:"template,omitempty"`
-	TemplatePath  string        `yaml:"templatePath,omitempty"`
-	PreChange     string        `yaml:"preChange,omitempty"`
-	PostChange    string        `yaml:"postChange,omitempty"`
-	TimeFrequency time.Duration `yaml:"timefrequency" validate:"-"`
+	Path         string `yaml:"path,omitempty" validate:"required"`
+	Template     string `yaml:"template,omitempty"`
+	TemplatePath string `yaml:"templatePath,omitempty"`
 }
