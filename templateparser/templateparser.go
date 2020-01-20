48adfb13eecf7153ec6f919479e0fb664cb291a5
Chris Johnson <chrisjohnson0@gmail.com>
2020-01-20 16:03:35 -0500

    Refactor templateparser to give more context during template issues

--- templateparser/templateparser.go
+++ templateparser/templateparser.go
@@ -2,23 +2,27 @@ package templateparser
 
 import (
 	"bytes"
-	"github.com/Masterminds/sprig"
-	"github.com/chrisjohnson/azure-key-vault-agent/resource"
 	"log"
 	"path/filepath"
 	"text/template"
+
+	"github.com/chrisjohnson/azure-key-vault-agent/resource"
+
+	"github.com/Masterminds/sprig"
 )
 
 func InlineTemplate(inline string, path string, resource resource.ResourceMap) string {
-	log.Printf("Parsing inline template")
 	// Read in the template
-	t := template.Must(template.New("inline").Funcs(sprig.TxtFuncMap()).Parse(inline))
+	t, err := template.New("inline").Funcs(sprig.TxtFuncMap()).Parse(inline)
+	if err != nil {
+		log.Fatalf("Error parsing template:\n%v\nError:\n%v\n", inline, err)
+	}
 
 	var buf bytes.Buffer
 	// Execute the template
-	err := t.Execute(&buf, resource)
+	err = t.Execute(&buf, resource)
 	if err != nil {
-		log.Panic(err)
+		log.Fatalf("Error executing template:\n%v\nResource:\n%v\nError:\n%v\n", inline, resource, err)
 	}
 
 	result := buf.String()
@@ -26,15 +30,17 @@ func InlineTemplate(inline string, path string, resource resource.ResourceMap) s
 }
 
 func TemplateFile(tpath string, path string, resource resource.ResourceMap) string {
-	log.Printf("Parsing template file %v", tpath)
 	// Read in the template file
-	t := template.Must(template.New(filepath.Base(tpath)).Funcs(sprig.TxtFuncMap()).ParseFiles(tpath))
+	t, err := template.New(filepath.Base(tpath)).Funcs(sprig.TxtFuncMap()).ParseFiles(tpath)
+	if err != nil {
+		log.Fatalf("Error parsing template:\n%v\nError:\n%v\n", tpath, err)
+	}
 
 	var buf bytes.Buffer
 	// Execute the template
-	err := t.Execute(&buf, resource)
+	err = t.Execute(&buf, resource)
 	if err != nil {
-		log.Panic(err)
+		log.Fatalf("Error executing template:\n%v\nResource:\n%v\nError:\n%v\n", tpath, resource, err)
 	}
 
 	result := buf.String()
