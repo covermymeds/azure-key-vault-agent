86148f40b8bd2ca56a565ae8b03f69c74e9cafaf
Chris Johnson <chrisjohnson0@gmail.com>
2020-02-20 12:05:56 -0500

    Add support for --config/-c and --text-logs command line flags

--- main.go
+++ main.go
@@ -7,24 +7,45 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+var configFile string
+var jsonLogs bool = true
+
 func init() {
-	// JSON Format customized to use _timestamp so it marshals first alphabetically
-	log.SetFormatter(&log.JSONFormatter{
-		FieldMap: log.FieldMap{
-			log.FieldKeyTime:  "_timestamp",
-		},
-	})
+	var textLogs bool
+	flag.BoolVar(&textLogs, "text-logs", false, "Change from JSON log output format to text")
+	flag.StringVar(&configFile, "config", "", "Read config from this file")
+	flag.StringVar(&configFile, "c", "", "Read config from this file")
+
+	flag.Parse()
+
+	if configFile == "" {
+		log.Fatalf("Missing --config/-c")
+	}
+
+	if textLogs {
+		jsonLogs = false
+	}
+
+	if jsonLogs {
+		// JSON Format customized to use _timestamp so it marshals first alphabetically
+		log.SetFormatter(&log.JSONFormatter{
+			FieldMap: log.FieldMap{
+				log.FieldKeyTime: "_timestamp",
+			},
+		})
+	}
 
 	var err error
 	err = authconfig.ParseEnvironment()
 	if err != nil {
-		log.Fatalf("failed to parse env: %v\n", err.Error())
+		log.Fatalf("AuthConfig: Failed to parse env: %v", err.Error())
 	}
 
 	err = authconfig.AddFlags()
 	if err != nil {
-		log.Fatalf("failed to parse flags: %v\n", err.Error())
+		log.Fatalf("AuthConfig: Failed to add flags: %v", err.Error())
 	}
+
 	flag.Parse()
 }
 
@@ -35,6 +56,5 @@ func main() {
 		}
 	}()
 
-	configwatcher.Watcher("akva.yaml")
+	configwatcher.Watcher(configFile)
 }
-
