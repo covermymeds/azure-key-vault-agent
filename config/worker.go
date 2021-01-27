2933d90ceff15d3f17b2fad2b715720c1137a149
Chris Rees <crees@covermymeds.com>
2021-01-27 10:35:42 -0500

    Stop requiring frequency, allow default of 60

--- config/worker.go
+++ config/worker.go
@@ -6,7 +6,7 @@ import (
 
 type WorkerConfig struct {
 	Resources     []ResourceConfig `yaml:"resources" validate:"required,dive,required"`
-	Frequency     string           `yaml:"frequency,omitempty" validate:"required"`
+	Frequency     string           `yaml:"frequency,omitempty"`
 	TimeFrequency time.Duration    `yaml:"timefrequency" validate:"-"`
 	PreChange     string           `yaml:"preChange,omitempty"`
 	PostChange    string           `yaml:"postChange,omitempty"`
